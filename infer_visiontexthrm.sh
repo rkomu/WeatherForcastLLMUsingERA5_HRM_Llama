e623c201f7844c537ff6b6102fe01fd5c0908988
rkomu <ryukomura@icloud.com>
2025-08-17 13:30:59 +0900

    update shellscript

--- infer_visiontexthrm.sh
+++ infer_visiontexthrm.sh
@@ -2,11 +2,13 @@
 
 python -m vision_text.infer_hrm_vision2text \
   --mae_ckpt checkpoints/satswinmae_epoch10.pt \
-  --adapter_ckpt checkpoints_v2t/adapter_epoch20.pt \
+  --adapter_ckpt checkpoints_v2t/adapter_epoch54.pt \
   --files ./dataset/raw_data/**/*.nc \
   --variables u10 v10 r sp ssrd t cp \
-  --date 2025-08-01 \
-  --anchor last \
-  --window_T 8 --window_H 64 --window_W 64 \
-  --stride_T 8 --stride_H 64 --stride_W 64 \
+  --date 2024-08-02 \
+  --time_start "2024-08-02" \
+  --time_end   "2024-08-04" \
+  --anchor first \
+  --window_T 2 --window_H 64 --window_W 64 \
+  --stride_T 1 --stride_H 64 --stride_W 64 \
   --max_samples 4
--- train_mae.sh
+++ train_mae.sh
@@ -2,7 +2,7 @@
 
 python3 -m sat_swin_mae.train_mae \
   --files "./dataset/raw_data/**/*.nc" \
-  --batch_size 128 --epochs 10 \
+  --batch_size 128 --epochs 5 \
   --val_ratio 0.3 --split_mode random --seed 123 \
   --window_T 8 --window_H 64 --window_W 64 \
   --stride_T 4 --stride_H 32 --stride_W 32 \
@@ -11,7 +11,7 @@ python3 -m sat_swin_mae.train_mae \
   --patch_t 2 --patch_h 4 --patch_w 4 \
   --variables u10 v10 r sp ssrd t cp \
   --time_start "2024-08-01" \
-  --time_end   "2024-08-30" \
+  --time_end   "2024-08-31" \
   --mlflow_experiment_name "era5_pretraining" \
   --mlflow_run_name "swinmae_v2.1" \
   --mlflow_tags dataset=era5_2024 gpu=3090 experiment_type=baseline \
